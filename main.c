692f30e421f9c6b3e75d513e6f725e13f9e317f3
cilinhosan <cilinhosan@hotmail.com>
2017-07-22 00:49:36 -0300

    Funcionalidades ok

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ void nfgets(char put_array[], int max_size); //nfgets function
 int register_window(Account create[], int acc_number);//tela de registro. Funcao register_window retorna 1 se teve sucesso
 void line_start();
 void line_end();
+int check_at(char string[]);//retorna 1 se o @ esta presente e -1 se nao
 //
 int main()
 {
@@ -35,25 +36,42 @@ int main()
 int register_window(Account create[], int acc_number)
 {
     char char_temp_info[7][30];
-
+    int flag = 0;
     printf("email: ");
-    nfgets(char_temp_info[0], 30);
-    //se nao tiver @ manda digitar denovo "email invalido digite novamente"
-    //
-    //se tiver correto
-    strcpy(create[acc_number].email, char_temp_info[0]);
+    do
+    {
+        nfgets(char_temp_info[0], 30);
+        //se nao tiver @ manda digitar denovo "email invalido digite novamente"
+        if( ( check_at(char_temp_info[0]) ) != 1){
+            printf("email invalido, digite novamente\n");
+        }
+        else{
+            //se tiver correto
+            strcpy(create[acc_number].email, char_temp_info[0]);
+            flag = 1;
+        }
+    }while(flag != 1);
     //
     printf("login: ");
     nfgets(char_temp_info[1], 30);
     strcpy(create[acc_number].login, char_temp_info[1]);
 
-    printf("senha: ");
-    nfgets(char_temp_info[2], 30);
-    //
-    printf("confirme a senha: ");
-    nfgets(char_temp_info[3], 30);
-    //se estiverem iguais
-    strcpy(create[acc_number].password, char_temp_info[3]);
+    flag = 0;
+    do{
+        printf("senha: ");
+        nfgets(char_temp_info[2], 30);
+        //
+        printf("confirme a senha: ");
+        nfgets(char_temp_info[3], 30);
+        //se estiverem iguais
+        if(strcmp(char_temp_info[2], char_temp_info[3]) == 0){
+            strcpy(create[acc_number].password, char_temp_info[3]);
+            flag = 1;
+        }
+        else{
+            printf("senhas nao estao diferentes, digite novamente\n");
+        }
+    }while(flag != 1);
 
     printf("pergunta de seguranca: ");
     nfgets(char_temp_info[4], 30);
@@ -66,6 +84,24 @@ int register_window(Account create[], int acc_number)
     //se conta criada
     create[acc_number].status = 1;
     return 1;
+}
+
+int check_at(char get_string[])
+{
+    int size = strlen(get_string);
+    int found = 0;
+    int i = 0;
+    for(i = 0; i < size; i++){
+        if(get_string[i] == '@'){
+            found++;
+        }
+    }
+    if(found == 1){
+        return 1;
+    }
+    else{
+        return -1;
+    }
 }
 
 void nfgets(char put_array[], int max_size)
